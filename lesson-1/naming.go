54e5dc1b051df3b496b52e16205668d3fdbaafbc
Dmitry <dmitry@Andreys-MacBook-Air.local>
2020-04-17 18:43:36 +0300

    upd

---
+++ lesson-1/naming.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+	// camelCase - принятый стиль
+	var numValue int
+	// under_score - не принято
+	var num_value int
+
+	// переменная, хранящяя возраст сотрудника
+	var employeeAge int
+
+	// экспортируемая переменная
+	var Variable int
+	// неэкспортируемая переменная
+	var variable int
+}
--- lesson-1/style.go
+++
@@ -1,8 +0,0 @@
-package main
-
-func main() {
-	// camelCase - принятый стиль
-	var numValue int
-	// under_score - не принято
-	var num_value int
-}
